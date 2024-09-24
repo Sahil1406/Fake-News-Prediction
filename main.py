d2574588860feb7aecfe42d06721c4e8d9a7cf19
Sahil <sahilsuryawanshi76@gmail.com>
2024-09-24 23:28:26 +0530

    stemming is still left

--- main.py
+++ main.py
@@ -10,4 +10,45 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-print(stopwords.words('English'))
+# print(stopwords.words('English'))
+
+dataset = pd.read_csv('train.csv')
+
+# CHECKING OUT THE DATA
+
+# print(dataset.shape)
+# print(dataset.head())
+# print(dataset.isnull().sum())
+
+# REPLACING NULL VALUES WITH EMPTY STRING
+
+dataset = dataset.fillna('')
+
+# MERGING THE AUTHOR TITLE AND TEXT TOGETHER
+
+dataset['content'] = dataset['author']+' '+ dataset['title']+' '+ dataset['text']
+
+# print(dataset['content'])
+
+# SEPARATING THE USEFUL DATA AND LABELS
+
+X = dataset.drop('label', axis=1)
+Y = dataset['label']
+
+# print(X)
+# print(Y)
+
+# STEMMING 
+port_stem = PorterStemmer()
+
+def stemming(content):
+    stemmed_content = re.sub('[^a-zA-Z]', ' ', content) 
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
+
+dataset['content'] = dataset['content'].apply(stemming)
+
+print('done')
